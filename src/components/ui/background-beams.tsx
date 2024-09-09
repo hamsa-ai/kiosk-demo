68854c59992645e16fbae59a101400deae4e8103
Noor Aldeen <nooraldeen.bayari@nadsoft.net>
2024-09-09 14:26:43 +0300

    fix(ts): removed unused variable

--- src/components/ui/background-beams.tsx
+++ src/components/ui/background-beams.tsx
@@ -80,7 +80,7 @@ export const BackgroundBeams = React.memo(
             />
           ))}
           <defs>
-            {paths.map((path, index) => (
+            {paths.map((_, index) => (
               <motion.linearGradient
                 id={`linearGradient-${index}`}
                 key={`gradient-${
