4acfa973baa841f413a05b17c2f7e501c3af68c4
Noor Bayari <35839097+NoorBayari@users.noreply.github.com>
2025-07-01 09:56:20 +0300

    feat: move API key to environment variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vitest/globals" />
+
+interface ImportMetaEnv {
+  readonly VITE_HAMSA_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
